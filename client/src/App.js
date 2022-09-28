90a0f372324b841f3266890ea587e3728cc07548
Jhon coneo <jconeo117@gmail.com>
2022-09-28 18:55:59 -0300

    fix(src):store provider en componente index

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-
+import React from 'react'
+import {useDispatch} from 'react-redux'
+import { GetAllRooms, GetRoom } from './Redux/Actions'
 
 function App() {
+  
+  const Dispatch = useDispatch()
+  
+
+  function HandleRooms(){
+    Dispatch(GetAllRooms())
+  }
+
+  function HandleRoom(){
+    Dispatch(GetRoom())
+  }
+
   return (
     <div className="App">
-      SnorInnApp
+      <h1>SnorInnApp</h1>
+      <button onClick={HandleRooms}>traer una habitacion</button>
+      <button onClick={HandleRoom}>traer todas habitacion</button>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './Redux/Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
