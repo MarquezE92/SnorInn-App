286c05574620c0a614f3dcead7930acfe3fb99e8
Jhon coneo <jconeo117@gmail.com>
2022-09-28 18:51:44 -0300

    feature(Redux):Redux actions, reducer y store base

---
+++ client/src/Redux/Actions/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+
+export function GetAllRooms(){
+    console.log('esta funcion trae todas las habitaciones')
+}
+
+export function GetRoom(){
+    console.log('esta funcion trae una sola habitacion')
+}
