5ad7a546de0d9cdc5d2feb2f76cade4e9f8193cc
MarquezE92 <vsuykyasmbp@hotmail.com>
2022-09-29 20:03:23 -0300

    feat(client) add ruteo RoomCard and Roomdetail to render in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import RoomForm from "./Admin/Features/RoomForm";
 import NavBar from "./Shared/Navbar";
+import RoomCard from "./User/Features/RoomCard";
+import RoomDetail from "./User/Features/RoomDetail";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <NavBar/>
     <Routes>
       <Route path= '/create' element={<RoomForm/>}/>
+      <Route path= '/rooms' element={<RoomCard/>}/>
+      <Route path= '/detail' element={<RoomDetail/>}/>
     </Routes>
     </>
   );
