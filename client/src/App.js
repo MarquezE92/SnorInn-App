cebd9b7347d3356c2b3e6381c278f94c072b29c2
MarquezE92 <vsuykyasmbp@hotmail.com>
2022-09-29 01:02:48 -0300

    feat(client): add NavBar to render in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import RoomForm from "./Admin/Features/RoomForm";
+import NavBar from "./Shared/Navbar";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
  
   return (
     <>
+    <NavBar/>
     <Routes>
       <Route path= '/create' element={<RoomForm/>}/>
     </Routes>
