84636523b44bd8879228ec51781fda3b32fb9a72
Franco Nahuel Moreno <morenofranco1999@gmail.com>
2022-09-29 01:40:58 -0300

    create(api): new realtions

--- api/src/db.js
+++ api/src/db.js
@@ -1,2 +1,15 @@
 // Aca en este archivo deberian ir las tablas relacionales y los pedidos a Sequilize en el PI, 
 // pero al ser una DB de ATLAS en la Nube, no se necesita completar ya que el pedido es en index.js
+// const Admin = require('./models/Admin')
+// const Reviews = require('./models/Reviews')
+// const Reservation = require('./models/Reservation')
+const roomSchema = require('./models/roomSchema')
+// const User = require('./models/User')
+
+module.exports = {
+    // Admin,
+    // Reviews,
+    roomSchema,
+    // User,
+    // Reservation
+}
