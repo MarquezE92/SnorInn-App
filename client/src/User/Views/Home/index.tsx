9c1141374f4084cfe0d3f15c267997ca8b3612cd
Jhon coneo <jconeo117@gmail.com>
2022-10-02 12:50:43 -0300

    update(Home):home page states

--- client/src/User/Views/Home/index.tsx
+++ client/src/User/Views/Home/index.tsx
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState, ChangeEvent} from "react";
 import style from './Home.module.css'
 
 
+
 const Home =()=>{
+
+    // const dispatch = useAppDispatch()
+    // const rooms = useAppSelector((state)=>state.rooms)
+    
+    const [select, setSelect] = useState<Object>({
+        place:'',
+        name:'',
+        n_beds:'',
+        category:''
+    })
+
+
+    const findBy=(e:ChangeEvent<HTMLSelectElement>)=>{
+        setSelect({
+            ...select,
+            [e.target.name] : [e.target.value]
+        })
+
+    }
+
+    const searchBy = (e:any)=>{
+        e.preventDefault()
+       
+    }
+
+    
+
+
+   
 
     return(
         <div className={style.fondo}>
             <div className={style.card}>
                 <form>
                   
-                    <select>
-                        <option disabled selected >PLACE</option>
-                        <option value="provincia1">Bogotá</option>
-                        <option value="provincia2">Buenos Aires</option>
-                        <option value="provincia3">Montevideo</option>
-                        <option value="provincia4">Lima</option>
+                    <select name="place" onChange={findBy}>
+                       ñ
                     </select>
                   
-                    <select>
+                    <select name="n_beds" onChange={findBy}>
                         <option disabled selected>N° BEDS</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -25,14 +51,14 @@ const Home =()=>{
                         <option value="4">4</option>
                     </select>
                     
-                    <select>
+                    <select name="category" onChange={findBy}>
                         <option disabled selected>CATEGORY</option>
                         <option value="basic">Basic</option>
                         <option value="standard">Standard</option>
                         <option value="premiun">Premiun</option>
                     </select>
                     
-                    <button className={style.learn_more}>
+                    <button className={style.learn_more} onClick={searchBy}>
                         <span className={style.circle} aria-hidden="true">
                         <span id={style.arrow} className={style.icon}></span>
                         </span>
