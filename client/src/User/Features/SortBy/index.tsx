747828e1cacf54e6116b0f770e1e79c818b1beee
Jhon coneo <jconeo117@gmail.com>
2022-10-03 01:17:53 -0300

    update(Redux):More actions for Redux

--- client/src/User/Features/SortBy/index.tsx
+++ client/src/User/Features/SortBy/index.tsx
@@ -1,24 +1,52 @@
-import React, {ChangeEvent} from 'react';
+import React, {ChangeEvent, useEffect} from 'react';
 import { useState } from 'react';
 import styles from './index.module.css';
 import SearchBar from '../../../Shared/Navbar/searchBar';
 
+import {useAppDispatch, useAppSelector} from '../../../Redux/Store/hooks'
+import { sortRoomsPrice, getRoomsByPage, sortRoomsRating} from '../../../Redux/slice/rooms'
+
 const SortBy = ()=> {
 
 	const [input, setInput] = useState('');
+    const dispatch = useAppDispatch()
+    const rooms =  useAppSelector((state)=>state.rooms)
 
+    
 	function handleSelect(e: ChangeEvent<HTMLSelectElement>) {
-		setInput(e.target.value)
+        setInput(e.target.value)
 	}
+    
+    function sortByPrice(e:ChangeEvent<HTMLSelectElement>){
+        e.preventDefault()
+        setInput(e.target.value)
+        dispatch(sortRoomsPrice(e.target.value))
+    }
+    
+    function sortByRating(e:ChangeEvent<HTMLSelectElement>){
+        e.preventDefault()
+        setInput(e.target.value)
+        dispatch(sortRoomsRating(e.target.value))
+    }
 
+    
+    useEffect(()=>{
+        dispatch(getRoomsByPage(1))
+    },[])
+
+    console.log('componente sort price',rooms.Rooms.map(el=>el.price))
+    console.log('componente sort rating',rooms.Rooms.map(el=>el.rating))
+    
 	return(
-		<div className={styles.sortDiv}>
-			<select onChange={handleSelect} className={styles.select}>
+        
+        <div className={styles.sortDiv}>
+            <select onChange={sortByPrice} className={styles.select}>
                 <option disabled selected >Order by price</option>
                 <option value="Cheaper">Cheaper</option>
                 <option value="More Expensive">More Expensive</option>
             </select>
-            <select onChange={handleSelect} className={styles.select}>
+
+            <select onChange={sortByRating} className={styles.select}>
                 <option disabled selected >Order by rating</option>
                 <option value="More Popular">More Popular</option>
                 <option value="Less Popular">Less Popular</option>
