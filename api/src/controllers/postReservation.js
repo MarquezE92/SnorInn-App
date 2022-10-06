9288d1a193354319c5cabae22b0c48abf438e1ac
FerPascualF50 <pascualefernando@gmail.com>
2022-10-05 23:37:53 -0300

    add(propertyPostReserv) Add vs

--- api/src/controllers/postReservation.js
+++ api/src/controllers/postReservation.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const { Reservation } = require('../db');
 
-const addResrevation = async ({ userId, roomId, check_in, check_out }) => {
+const addResrevation = async ({ userId, roomId, check_in, check_out, totalPrice }) => {
     const add = new Reservation({
         userId,
         roomId,
         check_in,
-        check_out
+        check_out,
+        totalPrice
     })
     const addNewSchema = await add.save()
     return addNewSchema
