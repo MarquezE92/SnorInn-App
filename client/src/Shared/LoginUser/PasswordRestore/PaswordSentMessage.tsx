fb880fcd37d47c6237414035bb7d002356e42e49
MarquezE92 <vsuykyasmbp@hotmail.com>
2022-10-07 13:46:53 -0300

    fix(client): Css and fix stripeForm price

--- client/src/Shared/LoginUser/PasswordRestore/PaswordSentMessage.tsx
+++ client/src/Shared/LoginUser/PasswordRestore/PaswordSentMessage.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./paswordSentMessage.module.css";
+import {BsFillEmojiWinkFill} from "react-icons/bs";
+import { useNavigate } from 'react-router-dom';
 
 const PaswordSentMessage = () => {
+
+	const navigate = useNavigate();
+
+	useEffect(
+  () => {
+    setTimeout(() => {navigate('/', {replace:true})}, 3000)
+  	}, []
+	);
+
 	return (
 		<div className={styles.mainDiv}>
       <div className={styles.divContainer}>
-        <h1>A mail with your new password has been sent! :)</h1>
+        <h1>A mail with your new password has been sent! <BsFillEmojiWinkFill/></h1>
       </div>
     </div>
 	)
