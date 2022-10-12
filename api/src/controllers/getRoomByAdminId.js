066317faac66b4f4c47ec2ec4f83fc1e95497179
FerPascualF50 <pascualefernando@gmail.com>
2022-10-11 23:36:44 -0300

    fix(SchemaUserAdmin) Fix(IndexController) Fix(controller) getRoomByID

--- api/src/controllers/getRoomByAdminId.js
+++ api/src/controllers/getRoomByAdminId.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
-const { UserClient } = require('../db');
+const { UserAdmin } = require('../db');
 
 //Modificamos el esquema de donde busca la info antes era 'userAdmin' ahora es 'UserClient'
 const getRoomsByUserAdmin = async (id) => {
     //const populateLog = {path: 'rooms.reservationId'}
-    const foundRoomByAdminId = await UserClient.findById(id).populate({
+    const foundRoomByAdminId = await UserAdmin.findById(id).populate({
         path: 'rooms',
         populate: { path: 'reservationId' }
-      });
+    });
     return foundRoomByAdminId
 };
 
