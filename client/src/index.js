225cea1c4754bcfaebc030d86d881e2bacc18eec
PabloLaMadrid <pablonicolaslm@gmail.com>
2022-10-13 14:10:33 -0300

    -

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {store} from './Redux/Store/store';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -17,9 +17,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <HashRouter >
+    <BrowserRouter>
     <App />
-    </HashRouter>
+    </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
