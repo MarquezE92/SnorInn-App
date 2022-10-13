abede9ee444b9dc1d03c8bf211f2a2c256221235
PabloLaMadrid <pablonicolaslm@gmail.com>
2022-10-12 23:21:27 -0300

    Redux restore

--- client/src/Redux/Store/store.ts
+++ client/src/Redux/Store/store.ts
@@ -1,30 +1,16 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import rooms from "../slice/rooms";
 //import authSlice from "../slice/authSlice"
 import users from '../slice/user'
 import admin from '../slice/authSlice'
-import {
-    persistReducer,
-  } from 'redux-persist'
-import storage from "redux-persist/lib/storage";
 
-
-const reducers = combineReducers({
-    rooms: rooms,
-    admin: admin,
-    users: users,
-})
-
-const persistConfig = {
-    key: 'root',
-    version: 1,
-    storage,
-  }
-
-const persistedReducer = persistReducer(persistConfig, reducers)
 
 export const store = configureStore({
-    reducer: persistedReducer
+    reducer:{
+       rooms,   
+       users,
+       admin
+    }
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
