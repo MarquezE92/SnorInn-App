ff59068b11c2ca0c1af7bcd7ad0e7cba40c41040
PabloLaMadrid <pablonicolaslm@gmail.com>
2022-10-13 00:22:34 -0300

    Reverse changes

--- client/src/index.js
+++ client/src/index.js
@@ -6,14 +6,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {store} from './Redux/Store/store';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { browserHistory } from 'react-router'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <Provider store={store}>
-    <BrowserRouter history={browserHistory}>
+    <BrowserRouter >
     <App />
     </BrowserRouter>
     </Provider>
