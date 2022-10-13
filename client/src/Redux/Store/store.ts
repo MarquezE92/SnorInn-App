04b7dfe1976d92a234835aff74d2c3cf53c4470f
PabloLaMadrid <pablonicolaslm@gmail.com>
2022-10-12 23:11:57 -0300

    Feat(redux): redux persist

--- client/src/Redux/Store/store.ts
+++ client/src/Redux/Store/store.ts
@@ -1,16 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import rooms from "../slice/rooms";
 //import authSlice from "../slice/authSlice"
 import users from '../slice/user'
 import admin from '../slice/authSlice'
+import {
+    persistReducer,
+  } from 'redux-persist'
+import storage from "redux-persist/lib/storage";
 
+
+const reducers = combineReducers({
+    rooms: rooms,
+    admin: admin,
+    users: users,
+})
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+  }
+
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 export const store = configureStore({
-    reducer:{
-       rooms,   
-       users,
-       admin
-    }
+    reducer: persistedReducer
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
--- client/src/index.js
+++ client/src/index.js
@@ -6,13 +6,20 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {store} from './Redux/Store/store';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { PersistGate } from 'redux-persist/integration/react'
+import {persistStore} from 'redux-persist';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let persistor = persistStore(store)
+
+
 root.render(
   <>
     <Provider store={store}>
     <BrowserRouter>
+    <PersistGate persistor = {persistor}>
     <App />
+    </PersistGate>
     </BrowserRouter>
     </Provider>
   </>
