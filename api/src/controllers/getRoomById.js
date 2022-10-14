242cd3912801aab34eaefdab2da517410ab8b370
FerPascualF50 <pascualefernando@gmail.com>
2022-10-14 14:19:43 -0300

    Add(Controler)GetRoom By Id Method

--- api/src/controllers/getRoomById.js
+++ api/src/controllers/getRoomById.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const express = require('express');
 router.use(express.json());
+const {roomSchema} = require('../db')
 
 const findByIdRoom = async (id) => {
     const find = await roomSchema.findById(id)
