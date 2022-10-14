1bea63253cd6bacc8f7d9ecc1096aeedaa892fce
FerPascualF50 <pascualefernando@gmail.com>
2022-10-14 15:01:19 -0300

    fix(FavoritesControllerDelete)Parameter

--- api/src/controllers/deteleFavorites.js
+++ api/src/controllers/deteleFavorites.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const { UserClient } = require('../db');
 
-const delFavorite = async ({idUserClient, roomFavorites}) =>{
-    const getIdClient = await UserClient.findById(idUserClient)
+const delFavorite = async ({idClient, idRoom}) =>{
+    const getIdClient = await UserClient.findById(idClient)
     getIdClient.roomFavorites =  getIdClient.roomFavorites.filter(e => {
-        return e.toString() !== roomFavorites
+        return e.toString() !== idRoom
     })
     await getIdClient.save()
     return getIdClient
