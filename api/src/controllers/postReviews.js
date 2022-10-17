d8afacc1dc696fde295484f46d144dfda2743396
MarquezE92 <vsuykyasmbp@hotmail.com>
2022-10-17 02:29:16 -0300

    fix(api): comment validation addReview

--- api/src/controllers/postReviews.js
+++ api/src/controllers/postReviews.js
@@ -3,10 +3,11 @@ const { roomSchema, ReviewRoom, UserClient, Reservation } = require('../db');
 
 const addReview = async ({userId, reservationId, roomId, stars, comment}) => {
     const getRooms = await roomSchema.findById(roomId)
-    const validate = await ReviewRoom.find({userId})
+  //  const validate = await ReviewRoom.find({userId})
     const getReservation =await Reservation.find({_id: reservationId, userId, roomId})
+    
     if(!getReservation.length) throw new Error('You do not have a reservation, you need it');
-    if(validate.length) throw new Error('you had a coment in this room');
+   // if(validate.length) throw new Error('you had a coment in this room');
     const addNewReview = new ReviewRoom({
         userId,
         reservationId,
