0e60010b45b2552573cb2d9aa31af6039ba4cd66
PabloLaMadrid <pablonicolaslm@gmail.com>
2022-10-17 19:59:06 -0300

    -

--- client/src/routes/authRoutes.tsx
+++ client/src/routes/authRoutes.tsx
@@ -4,6 +4,6 @@ import {Navigate, Outlet} from 'react-router-dom'
 export const AuthRoutes = ()=>{
     const admin = useAppSelector((state)=>state.admin.AdminInfo)
     return admin && admin._id !=''? 
-    <Outlet/> : (alert('You are not authorized'), <Navigate replace to={'/'}/>)
+    <Outlet/> : (alert('You are not authorized '), <Navigate replace to={'/'}/>)
 }
 
