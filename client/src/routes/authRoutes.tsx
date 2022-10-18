9ef33669317f7495ddd62e1803786ad87ce03430
MarquezE92 <vsuykyasmbp@hotmail.com>
2022-10-18 02:46:58 -0300

    fix(client/authRoutes): change alert

--- client/src/routes/authRoutes.tsx
+++ client/src/routes/authRoutes.tsx
@@ -1,9 +1,10 @@
-import {useAppSelector} from '../Redux/Store/hooks'
-import {Navigate, Outlet} from 'react-router-dom'
+import {useAppSelector} from '../Redux/Store/hooks';
+import {Navigate, Outlet} from 'react-router-dom';
+import Swal from "sweetalert2";
 
 export const AuthRoutes = ()=>{
     const admin = useAppSelector((state)=>state.admin.AdminInfo)
     return admin && admin._id !=''? 
-    <Outlet/> : (alert('You are not authorized'), <Navigate replace to={'/'}/>)
+    <Outlet/> : (Swal.fire("Ups!", "You are not authorized", "error"), <Navigate replace to={'/'}/>)
 }
 
